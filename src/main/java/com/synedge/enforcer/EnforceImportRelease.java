f8aa563f38d978260be80a5388e2bf8952cf6f88
Paul van Assen <source@pvanassen.nl>
2015-09-24 11:02:07 +0200

    Actual rule

---
+++ src/main/java/com/synedge/enforcer/EnforceImportRelease.java
@@ -0,0 +1,84 @@
+package com.synedge.enforcer;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+
+/**
+ * @author <a href="mailto:brianf@apache.org">Brian Fox</a>
+ * @author Paul van Assen
+ */
+public class EnforceImportRelease implements EnforcerRule {
+
+    public void execute(EnforcerRuleHelper helper)
+            throws EnforcerRuleException {
+        try {
+            // get the various expressions out of the helper.
+            MavenProject project = (MavenProject) helper.evaluate("${project}");
+            for (Object obj : project.getDependencyManagement().getDependencies()) {
+                Dependency dependency = (Dependency) obj;
+                if ("import".equalsIgnoreCase(dependency.getScope()) && dependency.getVersion().contains("-SNAPSHOT")) {
+                    throw new EnforcerRuleException("Found an artifact in the import scope containing SNAPSHOT!");
+                }
+            }
+        } catch (ExpressionEvaluationException e) {
+            throw new EnforcerRuleException("Unable to lookup an expression " + e.getLocalizedMessage(), e);
+        }
+    }
+
+    /**
+     * If your rule is cacheable, you must return a unique id when parameters or conditions
+     * change that would cause the result to be different. Multiple cached results are stored
+     * based on their id.
+     * <p>
+     * The easiest way to do this is to return a hash computed from the values of your parameters.
+     * <p>
+     * If your rule is not cacheable, then the result here is not important, you may return anything.
+     */
+    public String getCacheId() {
+        return "";
+    }
+
+    /**
+     * This tells the system if the results are cacheable at all. Keep in mind that during
+     * forked builds and other things, a given rule may be executed more than once for the same
+     * project. This means that even things that change from project to project may still
+     * be cacheable in certain instances.
+     */
+    public boolean isCacheable() {
+        return false;
+    }
+
+    /**
+     * If the rule is cacheable and the same id is found in the cache, the stored results
+     * are passed to this method to allow double checking of the results. Most of the time
+     * this can be done by generating unique ids, but sometimes the results of objects returned
+     * by the helper need to be queried. You may for example, store certain objects in your rule
+     * and then query them later.
+     */
+    public boolean isResultValid(EnforcerRule arg0) {
+        return false;
+    }
+}
